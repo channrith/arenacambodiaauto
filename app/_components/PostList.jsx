5e91ed6e68b6e791d5762500e334442b6ff3d7f9
Moul Channrith <moul.channrith@passapptech.com.kh>
2025-10-14 23:51:09 +0700

    improve post list

--- app/_components/PostList.jsx
+++ app/_components/PostList.jsx
@@ -3,7 +3,7 @@ import PostItem from "./PostItem";
 const PostList = ({ posts, latest }) => {
     return (
         <div className="blog-posts">
-            {latest === true && <h3 className="text-2xl font-semibold mb-4">Latest Blog Posts</h3>}
+            {latest === true && <h3 className="text-2xl font-semibold mb-4">ព័ត៌មានចុងក្រោយ</h3>}
             {posts.map((post) => (
                 <PostItem key={post.id} post={post} />
             ))}
