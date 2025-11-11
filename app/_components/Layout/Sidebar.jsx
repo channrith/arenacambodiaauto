6650015b2eb73038369ef46a4959520695a66a93
Moul Channrith <moul.channrith@passapptech.com.kh>
2025-11-11 11:17:45 +0700

    update sidebar to fetch data from api

--- app/_components/Layout/Sidebar.jsx
+++ app/_components/Layout/Sidebar.jsx
@@ -5,62 +5,85 @@ import { usePathname } from "next/navigation";
 import Advertisement from "../Advertisement";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+
+async function getCarMakers() {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_GATEWAY_URL}/api/vehicle/makers?limit=24&service=acauto`, {
+            headers: {
+                "Content-Type": "application/json",
+                token: process.env.NEXT_PUBLIC_API_ACCESS_TOKEN || "", // optional token if your gateway requires it
+            },
+            next: { revalidate: 60 }, // ISR: cache for 1 minute
+        });
+
+        if (!res.ok) throw new Error("Failed to fetch news");
+        const data = await res.json();
+
+        return data;
+    } catch (err) {
+        console.error("❌ Error loading news:", err);
+        return null;
+    }
+}
 
 const Sidebar = () => {
     const pathname = usePathname();
-    const makers = [
-        { brand: "toyota", path: "/make/toyota/local", label: "Toyota" },
-        { brand: "ssangyong", path: "/make/ssangyong/local", label: "SsangYong" },
-        { brand: "mitsubishi", path: "/make/mitsubishi/local", label: "Mitsubishi" },
-        { brand: "ford", path: "/make/ford/local", label: "Ford" },
-        { brand: "greely", path: "/make/greely/local", label: "Greely" },
-        { brand: "aiqar", path: "/make/aiqar/local", label: "AIQAR" },
-        { brand: "nissan", path: "/make/nissan/local", label: "Nissan" },
-        { brand: "chang-an", path: "/make/chang-an/local", label: "Chang an" },
-        { brand: "peugeot", path: "/make/peugeot/local", label: "Peugeot" },
-        { brand: "isuzu", path: "/make/isuzu/local", label: "Isuzu" },
-        { brand: "jeep", path: "/make/jeep/local", label: "Jeep" },
-        { brand: "gac", path: "/make/gac/local", label: "GAC" },
-        { brand: "hyundai", path: "/make/hyundai/local", label: "Hyundai" },
-        { brand: "range-rover", path: "/make/range-rover/local", label: "Land Rover" },
-        { brand: "chevrolet", path: "/make/chevrolet/local", label: "Chevrolet" },
-        { brand: "kia", path: "/make/kia/local", label: "KIA" },
-        { brand: "mazda", path: "/make/mazda/local", label: "Mazda" },
-        { brand: "honda", path: "/make/honda/local", label: "Honda" },
-        { brand: "suzuki", path: "/make/suzuki/local", label: "Suzuki" },
-        { brand: "jmc", path: "/make/jmc/local", label: "JMC" },
-        { brand: "mercedes", path: "/make/mercedes/local", label: "Mercedes" },
-        { brand: "mg", path: "/make/mg/local", label: "MG" },
-        { brand: "gtv", path: "/make/gtv/local", label: "GTV" },
-        { brand: "bmw", path: "/make/bmw/local", label: "BMW" },
-        { brand: "", path: "/logo", label: "All Brands" },
-    ];
+    const [makers, setMakers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const cached = localStorage.getItem("carMakers");
+        if (cached) {
+            setMakers(JSON.parse(cached));
+            setLoading(false);
+        } else {
+            (async () => {
+                const apiResponse = await getCarMakers();
+                const makersData = apiResponse.makers || [];
+                setMakers(makersData);
+                localStorage.setItem("carMakers", JSON.stringify(makersData));
+                setLoading(false);
+            })();
+        }
+    }, []);
 
     return (
         <aside className="sidebar">
             <div className="sidebar__top">
                 <h3>Car Producers</h3>
+                {loading ? (<p>Loading makers...</p>) : makers.length > 0 ? (
+                    <div className="maker">
+                        {makers.map((maker, index) => {
+                            const isActive = pathname.includes(maker.link);
+                            return (
+                                <Link
+                                    key={index}
+                                    href={maker.link}
+                                    className={`maker__link ${isActive ? 'active' : ''}`}
+                                >
+                                    {maker.path === "/logo" ? (
+                                        <>
+                                            <FontAwesomeIcon icon={faBars} /> {maker.alt}
+                                        </>
+                                    ) : (
+                                        maker.alt
+                                    )}
+                                </Link>
+                            )
+                        })}
+                        <Link
+                            href="/logo"
+                            className="maker__link"
+                        >
+                            <FontAwesomeIcon icon={faBars} />
+                            <span className="text-xs">All Brands</span>
+                        </Link>
+                    </div>
+                ) : (
+                    <p>No makers found.</p>
+                )}
 
-                <div className="maker">
-                    {makers.map(maker => {
-                        const activeMaker = makers.find(maker => pathname.includes(`/${maker.brand}/`));
-                        return (
-                            <Link
-                                key={maker.brand}
-                                href={maker.path}
-                                className={`maker__link ${activeMaker?.brand === maker.brand ? 'active' : ''}`}
-                            >
-                                {maker.path === "/logo" ? (
-                                    <>
-                                        <FontAwesomeIcon icon={faBars} /> {maker.label}
-                                    </>
-                                ) : (
-                                    maker.label
-                                )}
-                            </Link>
-                        )
-                    })}
-                </div>
             </div>
 
             <Advertisement
